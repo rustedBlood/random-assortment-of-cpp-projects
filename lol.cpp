690a46acca75b1d05850e7b087a889a5c3172179
Norad <davidWnguyen@proton.me>
2023-01-01 22:21:02 -0500

    test

--- lol.cpp
+++ lol.cpp
@@ -7,24 +7,24 @@
 
 int main()
 {
-    unique_ptr<player> razoe(new player);
+    unique_ptr<player> playerOne(new player);
     map<string, attribute_class> attribute_classes = generate_attribute_classes();
 
-    razoe -> add_attribute(1.5, "Damage Bonus", attribute_classes["damage_mult"]);
-    razoe -> add_attribute(0.5, "Damage Penalty", attribute_classes["damage_mult"]);
+    playerOne -> add_attribute(1.5, "Damage Bonus", attribute_classes["damage_mult"]);
+    playerOne -> add_attribute(0.5, "Damage Penalty", attribute_classes["damage_mult"]);
 
-    razoe -> add_attribute(40.0, "Base Damage Bonus", attribute_classes["base_damage"]);
-    razoe -> add_attribute(75.0, "Base Damage Bonus (Class Bonus)", attribute_classes["base_damage"]);
+    playerOne -> add_attribute(40.0, "Base Damage Bonus", attribute_classes["base_damage"]);
+    playerOne -> add_attribute(75.0, "Base Damage Bonus (Class Bonus)", attribute_classes["base_damage"]);
 
-    double damagePctMod = razoe -> accumulate_attribute(1.0, "damage_mult");
-    double damageBaseMod = razoe -> accumulate_attribute(0.0, "base_damage");
+    double damagePctMod = playerOne -> accumulate_attribute(1.0, "damage_mult");
+    double damageBaseMod = playerOne -> accumulate_attribute(0.0, "base_damage");
 
     cout << damagePctMod << " Damage Multiplier\n";
     cout << damageBaseMod << " Base Damage Modifier\n";
 
-    cout << (razoe -> damage + damageBaseMod) * damagePctMod << " Output Damage\n";
+    cout << (playerOne -> damage + damageBaseMod) * damagePctMod << " Output Damage\n";
 
-    razoe -> print_attributes();
+    playerOne -> print_attributes();
 
     return 0;
 }
